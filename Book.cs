08395b3ffc05958fa00d22072be76b2369489a26
Ryelasse <lass375s@edu.zealand.dk>
2023-10-06 11:04:51 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BookLib
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        private string _title;
+
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                if (value.Length >= 3)
+                {
+                    _title = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Titlen skal minimum have 3 bogstaver");
+                }
+            }
+        }
+
+        private double _price;
+
+        public double Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value >= 0 && value <= 1200)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Prisen skal være mellem 0 og 1200 kr");
+                }
+            }
+        }
+
+        public Book(int id, string title, double price)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+        }
+
+        public Book()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id.ToString()}, {nameof(Title)}={Title}, {nameof(Price)}={Price.ToString()}}}";
+        }
+    }
+}
---
+++ BookRepository.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLib
+{
+    public class BookRepository
+    {
+        public List<Book> books = new List<Book>()
+       {
+           (new Book(1, "Matematik for idioter", 450)),
+           (new Book(2, "Hvordan du får din kæreste til at give dig ret", 300)),
+           (new Book(3, "Get Rich", 600)),
+           (new Book(4, "skodbogen", 50)),
+           (new Book(5, "Stodderen og Millionæren", 200))
+       };
+       
+
+        public Book Add(Book book)
+        {
+            books.Add(book);
+            return book;
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            return new List<Book>(books);
+        }
+
+        public Book GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public Book GetFilterPrice()
+        {
+            foreach (var x in books)
+            {
+                if (250 <= x.Price)
+                {
+                    return x;
+                }
+            }
+            throw new Exception("Der er ingen bøger der opfylder priskravet");
+        }
+
+        public Book GetFilterTitle()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book? Delete(int id)
+        {
+            Book? deletedBook = books.Find(a => a.Id == id);
+            if (deletedBook != null)
+            {
+                books.Remove(deletedBook);
+            }
+            return deletedBook;
+        }
+
+        public Book? Update(int id, Book updatedBook)
+        {
+            
+            Book? existingBook = books.Find(b => b.Id == id);
+
+            if (existingBook != null)
+            {
+                
+                existingBook.Title = updatedBook.Title;
+                existingBook.Price = updatedBook.Price;
+
+               
+
+                return existingBook;
+            }
+
+            throw new ArgumentException("Book not found");
+        }
+
+
+
+
+    }
+}
